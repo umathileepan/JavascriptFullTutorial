14cc78495b11394d1b2d51522e7b069fc3ed286e
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2022-07-18 12:53:45 +0100

    using JavaScript to greeting.

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = "Uma Thileepan"
+let greeting = "Welcome Back"
+let welcomeEL = document.getElementById("welcome-el")
+welcomeEL.innerText = greeting + " " + name
+welcomeEL.innerText += " 👋🏾"
+
