50361202a53a0464182f0c59b554696bfbcaf35c
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2022-07-19 09:44:56 +0100

    done variable and function practice.

--- PeopleCount/script.js
+++ PeopleCount/script.js
@@ -1,14 +1,45 @@
+let countEL =document.getElementById("count-lbl")
 let count = 0
-let saveEL=document.getElementById("save-el")
-let countSep = saveEL.innerText
+let saveEL= document.getElementById("save-el")
+let firstname = "Uma"
+let lastname = "Thileepan"
+let fullname = firstname + " " + lastname
+let myPoints = 3
+
+function add3Points()
+{
+    myPoints += 3
+}
+
+function remove1Point()
+{
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+console.log(myPoints)
+
+document.getElementById("myName").textContent = fullname
+
+
 
 function increment()
 {
     count += 1
-    document.getElementById("count-lbl").innerText = count
+    countEL.textContent = count
 }
 
 function save()
 {
-    countSep.saveEL.innerText = count + " - "
+    let countStr = count + " - "
+    saveEL.textContent += countStr
+    countEL.textContent = 0
+    count = 0
 }
+
+console.log("LEt's count people on the subway!")
+
